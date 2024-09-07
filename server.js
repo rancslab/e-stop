b8f540ccf266ecd87b481048c5b832bbd6f0886d
Swan Thurein <thureinswanhtet7@gmail.com>
2024-09-06 23:24:34 -0400

    implemented basic backend and frontend

---
+++ server.js
@@ -0,0 +1,28 @@
+const dgram = require('dgram');
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const PORT = 3001;
+
+//Enbale CORS so your frontend can access this server
+app.use(cors());
+
+//Crate a UDP client
+const udpClient = dgram.createSocket('udp4');
+
+//Define an endpoint to handle the stop signal
+app.post('/send-stop-signal', (req, res) => {
+    const stopSignal = Buffer.from([1]); //1 is the stop signal
+
+    ///Send the UDP stop signal to the RSU
+    udpClient.send(stopSignal, 44002, '127.0.0.1', (err) => {
+        if (err) {
+          return res.status(500).send('Failed to send stop signal');
+        }
+        res.send('Stop signal sent successfully');
+      });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
+  const [statusMessage, setStatusMessage] = React.useState('Idle');
+
+  const handleStopClick = async () => {
+    try {
+      const response = await fetch('http://localhost:3001/send-stop-signal',{
+        method: 'POST',
+      });
+
+      if (!response.ok){
+        setStatusMessage('Failed to send stop signal');
+      }
+
+      setStatusMessage('Stop signal sent to RSU');
+      
+    } catch (error) {
+      setStatusMessage('Error sending STOP signal');
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="min-h-screen flex flex-col items-center justify-center bg-gray-100">
+      <div className="bg-white p-8 rounded-xl shadow-md text-center">
+        <h1 className="text-2xl font-bold mb-4">Emergency Stop</h1>
+        <button
+          className="bg-red-600 text-white py-2 px-4 rounded-xl hover:bg-red-800 transition duration-300"
+          type="button"
+          onClick={handleStopClick}
         >
-          Learn React
-        </a>
-      </header>
+          Send Stop Signal 
+        </button>
+
+        <div className="mt-6 text-4xl font-semibold">{statusMessage}</div>
+
+      </div>
     </div>
   );
 }
